8e87b848164699f19c03c605b398be329ce38017
Anh Linh <linhnguyenanh75@gmail.com>
2024-09-10 13:30:27 +0700

    light-dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,37 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { changeMode } from '../redux/app/app.slide';
 
 function Header() {
+
+     const mode = useAppSelector(state => state.app.mode) 
+     const dispatch = useAppDispatch();
+
+     useEffect(() => {
+          const body = document.querySelector("body");
+          if(body) body.setAttribute('data-bs-theme', mode)
+     }, [mode])
+
      return (
           <Navbar className="bg-body-tertiary">
                <Container>
                     <Navbar.Brand href="#home">HoiThuyVan Redux</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                         <Navbar.Text>
-                              Signed in as: <a href="#login">Mark Otto</a>
-                         </Navbar.Text>
+                         <Form.Check
+                              value={mode}
+                              onChange={(e) => dispatch(changeMode(e.target.value === "light" ? "dark" : "light"))}
+                              type="switch"
+                              id="custom-switch"
+                              label={
+                                   mode === "light"
+                                        ? <Navbar.Text>Light Mode</Navbar.Text>
+                                        : <Navbar.Text>Dark Mode</Navbar.Text>
+                              }
+                         />
                     </Navbar.Collapse>
                </Container>
           </Navbar>
