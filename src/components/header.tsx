b9a8d69d48eedff58ebd61c04836fdba77920dc5
Anh Linh <linhnguyenanh75@gmail.com>
2024-09-10 14:12:49 +0700

    redux persist state to localstorage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,8 @@ function Header() {
                     <Navbar.Collapse className="justify-content-end">
                          <Form.Check
                               value={mode}
-                              onChange={(e) => dispatch(changeMode(e.target.value === "light" ? "dark" : "light"))}
+                              defaultChecked={mode === "light" ? false : true}
+                              onChange={(e) => dispatch(changeMode(e.target.checked === true ? "dark" : "light"))}
                               type="switch"
                               id="custom-switch"
                               label={
